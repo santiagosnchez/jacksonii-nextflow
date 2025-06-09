81acd03dd89c9c50d983ebc5ee77ff1c06dd6c18
Santiago Sanchez <santiago.snchez@gmail.com>
2025-06-09 16:48:56 -0400

    chore: switches to test table and adds arguments for potential QC

--- run.sh
+++ run.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-nextflow -C nextflow.config run main.nf
+nextflow -C nextflow.config run main.nf \
+    --from_sra data/samples/test_SraRunTable.csv \
+    -resume
+    # -with-trace -with-timeline timeline.html -with-report -with-dag dag.svg \
+    # -w work --outdir results "$@"
