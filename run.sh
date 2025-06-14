4aae7090951181933e8f7a77e40340f6f02cf0ed
Santiago Sanchez <santiago.snchez@gmail.com>
2025-06-14 01:07:03 -0400

    fix: adds creating local data dir and switches to full sra table

--- run.sh
+++ run.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 mkdir -p ./.tmp
+mkdir -p ./data
 nextflow -C nextflow.config run main.nf \
-    --from_sra data/samples/test_SraRunTable.csv \
+    --from_sra SraRunTable.csv \
     -resume
     # -with-trace -with-timeline timeline.html -with-report -with-dag dag.svg \
     # -w work --outdir results "$@"
